216bcc65df0057d064faf4965a85436253e0c36a
Sujon Mia <mdabrahon926@gmail.com>
2025-11-17 02:59:51 +0600

    update

--- apps/payments/views.py
+++ apps/payments/views.py
@@ -59,8 +59,8 @@ class CreateCheckoutSessionView(APIView):
                     "quantity": 1,
                 }],
                 mode="payment",
-                success_url=f"https://oxdouge.vercel.app/cart?order_id={order_id}&session_id={{CHECKOUT_SESSION_ID}}",
-                cancel_url=f"https://oxdouge.vercel.app/cart?order_id={order_id}",
+                success_url=f"https://notoverland.com/cart?order_id={order_id}&session_id={{CHECKOUT_SESSION_ID}}",
+                cancel_url=f"https://notoverland.com/cart?order_id={order_id}",
             )
 
             order.stripe_checkout_session_id = session.id
